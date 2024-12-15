9a041a204151636b0fe8e3fbe6ad8879c999293f
Aushota1 <rombritvin9@gmail.com>
2024-12-15 15:40:49 +0300

    new commit + java killer fich

--- Main.java
+++ Main.java
@@ -1,22 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,200,123512,77,343443,55,0,2};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество элементов в массиве: ");
+        int arraySize = scanner.nextInt();
+
+        // Создаем массив
+        int[] array = new int[arraySize];
+
+        // Заполняем массив элементами, введенными пользователем
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("Введите элемент %d: ", i + 1);
+            array[i] = scanner.nextInt();
+        }
+
+        // Сортируем массив по возрастанию
         for(int i = 0; i < array.length; i++){
             for (int j = 0; j < array.length; j++){
-                int num1 = array[i];
                 if(array[i]<array[j]){
+                    int num1 = array[i];
                     array[i]=array[j];
                     array[j] = num1;
-
                 }
-
             }
-            
         }
-        for (int number:array){
+
+        // Выводим отсортированный массив на экран
+        System.out.println("Отсортированный массив:");
+        for (int number : array) {
             System.out.println(number);
         }
+        System.out.println("макс. в списке");
     }
-    
-
 }
